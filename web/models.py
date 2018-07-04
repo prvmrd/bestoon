8d96e1ceaa060c33f5f54e88ed0410fe17b549cb
prvmrd <prvmrd@gmail.com>
2018-07-04 22:29:42 +0430

    requirments.txt added

--- web/models.py
+++ web/models.py
@@ -8,3 +8,14 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,
     on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}    {}    {}".format(self.text, self.amount, self.date)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,
+    on_delete = models.CASCADE)
+    def __str__(self):
+        return "{}  {}  {}  ".format(self.text, self.amount, self.date)
